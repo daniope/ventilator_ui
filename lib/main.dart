e920fffc584afcf92eea03036768c551b6ccd9cd
daniope <daniela.iope@hotmail.com>
2023-12-17 15:34:34 -0300

    refactor: moves layout and logic of home page to specific script in pages/home/home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'dart:async';
-import 'dart:io' show Directory, Platform;
-import 'dart:math' as math;
-
-import 'package:collection/collection.dart';
-import 'package:path/path.dart' as path;
-import 'package:tuple/tuple.dart';
-
-import 'package:ventilator_ui/console/console.dart';
-import 'package:ventilator_ui/charts/charts.dart';
-import 'package:ventilator_ui/ventilation/ventilation.dart';
-
-const String _libName = 'ventilation';
-const String _libPath = 'ventilation-c/build/lib$_libName.so';
+import 'pages/home/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  const App({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ventilator',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Ventilator Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Timer? timer;
-  late final Ventilation _v;
-
-  late final List<Sample> _sampleBuffer;
-
-  final Console _console = new Console(); 
-  final Charts _charts   = new Charts();
-
-  _MyHomePageState() {
-    _v = Ventilation.create(_libPath);
-    _sampleBuffer = [];
-
-    timer = Timer.periodic(
-      const Duration(microseconds: 10),
-      _updateDataSource
-    );
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel;
-    _v.delete();
-    super.dispose();
-  }
-
-  void _updateDataSource(Timer timer) {
-    _v.update();
-
-    _sampleBuffer.add(_v.getSample());
-
-    if (_sampleBuffer.length == 1000) {
-      var pMean = _sampleBuffer.map((s) => s.item1).average;
-      var fMean = _sampleBuffer.map((s) => s.item2).average;
-      var vMean = _sampleBuffer.map((s) => s.item3).average;
-      _sampleBuffer.clear();
-
-      _charts.updateDataSource(pMean, fMean, vMean);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(10.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: _console,
-              flex: 2,
-            ),
-            Expanded(
-              child: _charts,
-              flex: 8,
-            ),
-          ],
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/pages/home/home.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+import 'package:ventilator_ui/charts/charts.dart';
+import 'package:ventilator_ui/console/console.dart';
+import 'package:ventilator_ui/ventilation/ventilation.dart';
+
+import 'package:collection/collection.dart';
+
+const String _libName = 'ventilation';
+const String _libPath = 'ventilation-c/build/lib$_libName.so';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Timer? _timer;
+  late final Ventilation _v;
+
+  late final List<Sample> _sampleBuffer;
+
+  final Console _console = new Console();
+  final Charts _charts = new Charts();
+
+  @override
+  void initState() {
+    _v = Ventilation.create(_libPath);
+    _sampleBuffer = [];
+
+    _timer = Timer.periodic(
+      const Duration(microseconds: 10),
+      _updateDataSource
+    );
+
+    super.initState();
+  }
+
+  void _updateDataSource(Timer timer) {
+    _v.update();
+
+    _sampleBuffer.add(_v.getSample());
+
+    if (_sampleBuffer.length == 1000) {
+      var pMean = _sampleBuffer.map((s) => s.item1).average;
+      var fMean = _sampleBuffer.map((s) => s.item2).average;
+      var vMean = _sampleBuffer.map((s) => s.item3).average;
+      _sampleBuffer.clear();
+
+      _charts.updateDataSource(pMean, fMean, vMean);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: _console,
+              flex: 2,
+            ),
+            Expanded(
+              child: _charts,
+              flex: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
